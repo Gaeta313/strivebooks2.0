ccc266806032a13ddb0cac1c5c0af8b8b9ee7928
Gaeta313 <gaeta313@hotmail.it>
2022-08-31 17:00:11 +0200

    Esercizio Finito

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -2,15 +2,41 @@ import { Component } from "react";
 import { Card } from "react-bootstrap";
 
 class SingleBook extends Component {
+
+    state={
+        selected: false
+    }
+
   render() {
-    return (
-      <Card className="h-100">
+    if (!this.state.selected){
+        return (
+      <Card className="h-100" onClick={()=>{
+        let nuovo = this.state.selected;
+        nuovo = !nuovo;
+        this.setState({selected:nuovo});
+      }}>
         <Card.Img height="70%" variant="top" src={this.props.book.img} />
         <Card.Body >
           <Card.Title>{this.props.book.title}</Card.Title>
         </Card.Body>
       </Card>
     );
+    }
+    else{
+        return (
+            <Card border="danger" className="h-100" onClick={()=>{
+                let nuovo = this.state.selected;
+                nuovo = !nuovo;
+        this.setState({selected:nuovo});
+            }}>
+              <Card.Img height="70%" variant="top" src={this.props.book.img} />
+              <Card.Body >
+                <Card.Title>{this.props.book.title}</Card.Title>
+              </Card.Body>
+            </Card>
+          );
+    }
+    
   }
 }
 
